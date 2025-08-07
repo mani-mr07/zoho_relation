5c34a298aeed9802cbbd601e9c1462d82e1ccf11
mani-mr07 <144305407+mani-mr07@users.noreply.github.com>
2025-08-08 01:42:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
       Member esakki=new Member("esakki",null,null,'M',null);
       Member nattar=new Member("nattar",null,null,'F',null);
       Member mahesh=new Member("mahesh",esakki,nattar,'M',null);
+        System.out.println("Hi Bro");
       ArrayList<Member> siblings=new ArrayList<>();
       siblings.add(esakki);
       Member mani=new Member("mani",esakki,nattar,'M',siblings);
